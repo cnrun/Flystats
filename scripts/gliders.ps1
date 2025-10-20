b962799729b534ca4a0b3e7b27d2252efdce9163
Éric Louvard <9567496+cnrun@users.noreply.github.com>
2025-10-20 09:31:08 +0000

    Cosmetics

--- scripts/gliders.ps1
+++ scripts/gliders.ps1
@@ -6,13 +6,12 @@ $FlightsByGliders = (Get-ChildItem . -Filter flights.*.json | Get-Content | Conv
 
 foreach ($GliderStat in $FlightsByGliders) {
     $MeasureSeconds = (Get-ChildItem . -Filter flights.*.json `
-    | Get-Content `
-    | ConvertFrom-Json).Items `
+    | Get-Content | ConvertFrom-Json).Items `
     | Select-Object -Property Glider,routeFreeFlight `
-    | ForEach-Object { if ($_.glider.nameCompact -eq $GliderStat.Name) { [TimeSpan]::ParseExact($_.routeFreeFlight.duration,"\P\T%h\H%m\M%s\S",$null).TotalSeconds} } `
-    # | Measure-Object -Sum | ForEach-Object { (New-TimeSpan -Seconds $_.Sum).ToString() }
+    | ForEach-Object { if ($_.glider.nameCompact -eq $GliderStat.Name) 
+        { [TimeSpan]::ParseExact($_.routeFreeFlight.duration,"\P\T%h\H%m\M%s\S",$null).TotalSeconds} } `
     | Measure-Object -Sum
     $ts = New-TimeSpan -Seconds $MeasureSeconds.sum
-    $tsString = "$($ts.Days * 24 + $ts.Hours):$($ts.Minutes):$($ts.Seconds)S"
-    Write-Host "Name:$($GliderStat.Name) Count: $($GliderStat.Count) Time:$tsString"
+    $tsString = "{0:D2}:{1:D2}:{2:D2}" -f $($ts.Days * 24 + $ts.Hours),$($ts.Minutes),$($ts.Seconds)
+    Write-Host "$($GliderStat.Name) ($($GliderStat.Count)) $tsString"
 }
