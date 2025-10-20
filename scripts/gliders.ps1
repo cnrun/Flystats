8121e4f467cc111c2dd65ba3a56ab3a6231fe371
Éric Louvard <9567496+cnrun@users.noreply.github.com>
2025-10-20 07:04:25 +0000

    gliders, flights count

--- scripts/gliders.ps1
+++ scripts/gliders.ps1
@@ -1,3 +1,5 @@
 [CmdletBinding()]
 param(
 )
+
+(Get-ChildItem . -Filter flights.*.json | Get-Content | ConvertFrom-Json).Items | Select-Object -Property Glider | ForEach-Object { $_.glider.nameCompact } | Group-Object | Format-Table -Property Count, Name
